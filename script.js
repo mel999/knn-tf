ebc463bf3a2b129ab2302c654d1601116e66e709
mel999 <49338662+mel999@users.noreply.github.com>
2024-02-29 18:42:32 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 "use strict";
 
 const project1Details = document.querySelector(".expandDetails");
-const linkShow = document.querySelector(".linkShow");
+const generate = document.querySelector(".generate");
+//from project1:     <input type="submit" value="Submit" class="linkShow" />
+const earningsEl = document.getElementById("earnings");
+earningsEl.textContent = "input";
 
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+/*
 const init = function () {
   project1Details.classList.add("hidden");
 };
@@ -11,3 +24,5 @@ init();
 linkShow.addEventListener("click", function () {
   project1Details.classList.remove("hidden");
 });
+
+*/
