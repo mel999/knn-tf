2808da96e19c7466cfecc6d5f747ca97dd15705f
mel999 <49338662+mel999@users.noreply.github.com>
2024-03-06 15:36:20 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
 "use strict";
 
-var commonjs = require("./commonjs");
-
+/*var commonjs = require("./commonjs");
 require("@tensorflow/tfjs-node");
-
-//const { tf } = require("@tensorflow/tfjs-node");
 const tf = require("@tensorflow/tfjs");
-
-//const { tf } = require("@tensorflow/tfjs");
-
 const loadCSV = require("./load-csv");
-const { shuffle } = require("lodash");
+const { shuffle } = require("lodash");*/
+document.title = "Mel's tech skills";
 
 const project1Details = document.querySelector(".expandDetails");
-const generate = document.querySelector(".generate");
-const earningsEl1 = document.getElementById("earningsY2");
+const earningPoints = document.querySelector(".earningPoints");
+const earningsEl1 = document.getElementById("earningsY1");
 const earningsEl2 = document.getElementById("earningsY2");
+const formElem = document.getElementById("form1");
+let e1 = document.getElementById("e1");
+let e2 = document.getElementById("e2");
 
-function myFunction() {
+formElem.addEventListener("submit", function (e) {
+  e1.textContent = earningsEl1.value;
+  e2.textContent = earningsEl2.value;
+  e.preventDefault();
+});
+
+earningPoints.disabled = true;
+function myFunction3() {
+  earningsEl1 && earningsEl2
+    ? (earningPoints.disabled = false)
+    : (earningPoints.disabled = true);
+}
+
+function myFunction2() {
+  //condition ? exprIfTrue : exprIfFalse
+  e0.classList.remove("gg");
+  e1.classList.remove("gg");
+  e2.classList.remove("gg");
+  e4.classList.remove("gg");
+}
+
+function myFunction1() {
   var x = document.getElementById("myDIV");
   if (x.style.display === "none") {
     x.style.display = "block";
@@ -26,17 +45,10 @@ function myFunction() {
   }
 }
 
-//console.log(document.querySelector(".message").textContent);
-//console.log("hello");
-
 /*
-const init = function () {
-  project1Details.classList.add("hidden");
-};
-init();
-
-linkShow.addEventListener("click", function () {
-  project1Details.classList.remove("hidden");
-});
-
-*/
+  generate.innerHTML;
+  generate.textContent = "100 and 200";
+  generate.style.color = "pink";
+  console.log("i am clicking");
+  console.log(generate.textContent);
+  */
